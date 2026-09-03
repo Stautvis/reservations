915048cba7d8af4726176fbc8c0b6652d1ec899d
tsteponavicius <tautvydas.steponavicius@cognizant.com>
2026-09-03 08:48:50 +0000

    build: Add type parameter naming convention

--- src/utils/result/src/classes/result.class.ts
+++ src/utils/result/src/classes/result.class.ts
@@ -1,11 +1,11 @@
 import type { Exception } from "@utils/exception";
 import { ResultErrorAccessException, ResultException } from "../exceptions";
 
-export class Result<T, E extends Exception = Exception> {
-  private readonly _value: T | null;
-  private readonly _error: E | null;
+export class Result<TValue, TException extends Exception = Exception> {
+  private readonly _value: TValue | null;
+  private readonly _error: TException | null;
 
-  private constructor(value: T | null, error: E | null) {
+  private constructor(value: TValue | null, error: TException | null) {
     if (value !== null && error !== null) {
       throw new ResultException("Result cannot have both value and error");
     }
@@ -14,30 +14,32 @@ export class Result<T, E extends Exception = Exception> {
     this._error = error;
   }
 
-  public static ok<T>(value: T): Result<T> {
-    return new Result<T, never>(value, null);
+  public static ok<TValue>(value: TValue): Result<TValue> {
+    return new Result<TValue, never>(value, null);
   }
 
-  public static fail<E extends Exception>(error: E): Result<never, E> {
-    return new Result<never, E>(null, error);
+  public static fail<TException extends Exception>(
+    error: TException,
+  ): Result<never, TException> {
+    return new Result<never, TException>(null, error);
   }
 
-  public unwrap(): T {
+  public unwrap(): TValue {
     if (this._value === null) {
       throw this._error;
     }
     return this._value;
   }
 
-  public isOk(): this is Result<T, never> {
+  public isOk(): this is Result<TValue, never> {
     return this._value !== null;
   }
 
-  public isFail(): this is Result<never, E> {
+  public isFail(): this is Result<never, TException> {
     return this._error !== null;
   }
 
-  public get error(): E {
+  public get error(): TException {
     if (this._error === null) {
       throw new ResultErrorAccessException();
     }
